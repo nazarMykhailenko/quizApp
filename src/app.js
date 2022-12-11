df2b8c437e709eeccb192cf2a9c2d2369dd9a767
Nazar Mykhailenko <9142359911n@gmail.com>
2022-12-11 14:37:34 +0000

    section-cover created

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
+// constants for extra-classes
+const LOCKED = `_locked`
+const ACTIVE = `_active`
+
 // burger
 const $headerIcon = document.querySelector(`.header__icon`)
 const $navigationBody = document.querySelector(`.navigation__body`)
+const $body = document.body
 
 $headerIcon.addEventListener(`click`, () => {
-	$navigationBody.classList.toggle(`_active`)
-	$headerIcon.classList.toggle(`_active`)
+	$navigationBody.classList.toggle(ACTIVE)
+	$headerIcon.classList.toggle(ACTIVE)
+	$body.classList.toggle(LOCKED)
 })
